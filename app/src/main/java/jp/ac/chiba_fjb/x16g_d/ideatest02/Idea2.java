016d0288eaf2fe9121eb02b8cc52099b0f3b5dc0
WIN_NT\x16g025 <x16g025@chiba-fjb.ac.jp>
2018-11-20 15:50:13 +0900

    トレンド追加(仮)

--- app/src/main/java/jp/ac/chiba_fjb/x16g_d/ideatest02/Idea2.java
+++ app/src/main/java/jp/ac/chiba_fjb/x16g_d/ideatest02/Idea2.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
@@ -19,15 +23,18 @@ public class Idea2 extends Fragment{
     public Idea2() {
         // Required empty public constructor
     }
-
+    private WebView mWebView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_idea2, container, false);
-    }
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_idea2, container, false);
+        mWebView = (WebView)v.findViewById(R.id.web_view);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://trends.google.co.jp/trends/?geo=JP");
+
+        return v;
     }
 }
+
