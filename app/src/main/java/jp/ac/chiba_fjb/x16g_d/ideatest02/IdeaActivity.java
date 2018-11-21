dd13a84bbb110e14f55066b8d9469c7278314a71
WIN_NT\x16g018 <x16g018@chiba-fjb.ac.jp>
2018-11-21 10:48:04 +0900

    name変更

--- app/src/main/java/jp/ac/chiba_fjb/x16g_d/ideatest02/IdeaActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/x16g_d/ideatest02/IdeaActivity.java
@@ -1,38 +1,20 @@
 package jp.ac.chiba_fjb.x16g_d.ideatest02;
 
-import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
-import android.support.v7.widget.Toolbar;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity{
+public class IdeaActivity extends AppCompatActivity{
     private CharSequence[] tabTitle = {"アイデア入力", "トレンド検索"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_idea);
 
         FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager())
         {
